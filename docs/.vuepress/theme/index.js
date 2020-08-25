38da63a31ee7732fde35dd234bb0a92da7496ed5
Raquib-ul Alam <alam.kanak@gmail.com>
2020-08-26 00:47:13 +0600

    Add medium-zoom

--- docs/.vuepress/theme/index.js
+++ docs/.vuepress/theme/index.js
@@ -1,6 +1,11 @@
-plugins: [
-    ['@vuepress/pwa', {
-        serviceWorker: true,
-        updatePopup: true
-    }]
-]
+module.exports = {
+    plugins: [
+        ['@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: true
+        }],
+        ['@vuepress/medium-zoom', {
+            selector: '.portfolio-item-layout .content img'
+        }]
+    ]
+}
