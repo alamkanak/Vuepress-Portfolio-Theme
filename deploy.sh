39c2a0d784ee6bc711dfbd036e72c0cefa0f843b
Raquib-ul Alam <alam.kanak@gmail.com>
2020-08-26 07:08:56 +0600

    Automate deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,24 @@ set -e
 # build
 npm run docs:build
 
-# navigate into the build output directory
-cd docs/.vuepress/dist
+# Clone the deployment repo
+git clone https://github.com/alamkanak/alamkanak.github.io.git gitpush
+cd gitpush
+
+# Delete all previous files in the repo
+find . -type f \! \( -regex "\.\/\..*" -or -name '.' \) -exec rm -f {} +
+find . -type d \! \( -regex "\.\/.*\/.*" -or -name '.' -or -name '.git' \)  -exec rm -r {} +
+cd ..
+
+# Copy new dist files to the repo
+cp -r docs/.vuepress/dist/. gitpush
+cd gitpush
+
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# Commit to git
 git add -A
 git commit -m 'Deploy'
 
@@ -22,4 +33,5 @@ git push -f https://github.com/alamkanak/alamkanak.github.io.git master
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+cd ..
+sudo rm -r gitpush
