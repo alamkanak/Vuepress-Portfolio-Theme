a4b05fef8020fbfe3f448df3a9f62b772869a9b1
Raquib-ul Alam <alam.kanak@gmail.com>
2020-08-29 01:27:26 +0600

    Remove under construction notice

--- docs/.vuepress/theme/layouts/Home.vue
+++ docs/.vuepress/theme/layouts/Home.vue
@@ -3,8 +3,7 @@
         <b-container>
             <b-row>
                 <b-col class="text-center my-5">
-                    <b-alert show class="mb-5">This site is currently under construction</b-alert>
-                    <img :src="$page.frontmatter.avatar" alt="avatar" class="avatar mb-5 medium-zoom">
+                    <img :src="$page.frontmatter.avatar" alt="avatar" class="avatar mb-5 mt-4 medium-zoom">
                     <h2 class="mb-4 display-4">Hi! I'm {{ $page.frontmatter.title}}</h2>
                     <div class="lead">
                         <Content />
