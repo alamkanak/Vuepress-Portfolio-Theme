c10a7571622ba53a913610af0bf3112dde25aab9
Raquib-ul Alam <alam.kanak@gmail.com>
2020-08-29 02:27:35 +0600

    Fix site 404 page

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>    
-        Error 404
+    <div class="container">    
+        <div class="row">
+            <div class="col-12 text-center py-5">
+                <h1 class="display-1">Error 404</h1>
+                <a :href="$site.base">Go home</a>
+            </div>
+        </div>
     </div>
 </template>
